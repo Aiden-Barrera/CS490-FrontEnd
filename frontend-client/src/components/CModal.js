86556e4d16d7cc35ade25a53aa4e14d2ec8ea183
Aiden Barrera <aidenbarrera@macbookpro.resource.campus.njit.edu>
2025-02-08 18:13:04 -0500

    Added Modal Component for Movie/Actor description

---
+++ frontend-client/src/components/CModal.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { Modal, Typography } from "antd";
+import axios from "axios";
+const { Title, Paragraph } = Typography
+
+const CModal = ({ info, isFilm, open, handleClose }) => {
+  const [selectedInfo, setSelectedInfo] = useState(null)
+  
+  useEffect(() => {
+    if (!info) return
+
+    const fetchInfo = async () => {
+      try {
+        isFilm 
+          ? await axios.get(`http://localhost:3001/movieDescription/${info.film_id}`).then(response => {setSelectedInfo(response.data)})
+          : await axios.get(`http://localhost:3001/actorDescription/${info.actor_id}`).then(response => {setSelectedInfo(response.data)})
+
+      } catch (err) {
+        console.log("Error")
+      }
+    }
+
+    fetchInfo()
+  }, [info])
+  
+  return (
+    <Modal title={isFilm ? `${selectedInfo?.[0]?.title}`: `${info?.first_name}'s Top Films`} open={open} footer={null} onCancel={handleClose} centered> 
+      <Paragraph>{isFilm ? `Description: ${selectedInfo?.[0]?.description}` : `${selectedInfo?.[0]?.title}: ${selectedInfo?.[0]?.rental_count} Rented`}</Paragraph>
+      <Paragraph>{isFilm ? `Released: ${selectedInfo?.[0]?.release_year}` : `${selectedInfo?.[1]?.title}: ${selectedInfo?.[1]?.rental_count} Rented`}</Paragraph>
+      <Paragraph>{isFilm ? `Genre: ${selectedInfo?.[0]?.name}` : `${selectedInfo?.[2]?.title}: ${selectedInfo?.[2]?.rental_count} Rented`}</Paragraph>
+      <Paragraph>{isFilm ? `Rated: ${selectedInfo?.[0]?.rating}` : `${selectedInfo?.[3]?.title}: ${selectedInfo?.[3]?.rental_count} Rented`}</Paragraph>
+      <Paragraph>{isFilm ? `Rented: ${info?.rented}` : `${selectedInfo?.[4]?.title}: ${selectedInfo?.[4]?.rental_count} Rented`}</Paragraph>
+    </Modal>
+  )
+
+}
+
+export default CModal
--- frontend-client/src/pages/Home.js
+++ frontend-client/src/pages/Home.js
@@ -2,12 +2,26 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios'
 import './../App.css'
 import { Flex, Card, Typography, Divider } from 'antd'
+import CModal from './../components/CModal.js'
 const { Title, Paragraph } = Typography
 
 
 const Home = () => {
   const [topFilms, setTopFilms] = useState([])
   const [topActors, setTopActors] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isFilm, setIsFilm] = useState(false)
+  const [data, setData] = useState(null)
+
+  const showModal = (item) => {
+    setData(item)
+    setIsModalOpen(true)
+  }
+
+  const handleClose = () => {
+    setIsModalOpen(false)
+    setData(null)
+  }
   
   useEffect(() => {
     axios.get('http://localhost:3001/movies/top5Movies')
@@ -38,7 +52,8 @@ const Home = () => {
         <Flex wrap gap="large" justify="center"> 
           {topFilms.map((item) => (
             <Card className="card-hover" hoverable bordered={false} size="small" style={{ width: "220px", boxShadow: "0 2px 5px black", textAlign: "center", margin: "10px",
-              transition: "transform 0.3s ease, box-shadow 0.3s ease"}}> 
+              transition: "transform 0.3s ease, box-shadow 0.3s ease"}}
+              onClick={() => {showModal(item); setIsFilm(true)}}> 
               <Title level={3}>{item.title}</Title>
               <Paragraph>Rented: {item.rented}</Paragraph>
             </Card>
@@ -53,13 +68,16 @@ const Home = () => {
         <Flex wrap gap="large" justify="center">
           {topActors.map((item) => (
             <Card className="card-hover" hoverable bordered={false} size="small" style={{ width: "220px", boxShadow: "0 2px 5px black", textAlign: "center", margin: "10px", 
-              transition: "transform 0.3s ease, box-shadow 0.3s ease"}}>
+              transition: "transform 0.3s ease, box-shadow 0.3s ease"}}
+              onClick={() => {showModal(item); setIsFilm(false)}}>
               <Title level={3}>{item.first_name} {item.last_name}</Title>
               <Paragraph>Movies: {item.movies}</Paragraph>
             </Card>
           ))}
         </Flex>
       </div>
+
+      <CModal info={data} isFilm={isFilm} open={isModalOpen} handleClose={handleClose}/>  
     </>
   )
 }
